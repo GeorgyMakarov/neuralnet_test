1e44164fd5a301f8321a48b223efc6956703c3e4
GeorgyMakarov <georgy.v.makarov@gmail.com>
2021-06-16 08:41:27 +0200

    update caret neuralnet tuning

--- 11_regression_tuning.R
+++ 11_regression_tuning.R
@@ -27,12 +27,14 @@ near_zero_vars <- nearZeroVar(cars, saveMetrics = T)
 temp_fact <- as.data.frame(t(combn(colnames(cars)[3:18], 2)))
 colnames(temp_fact) <- c("f1", "f2")
 temp_vect <- c()
+
 for (i in 1:nrow(temp_fact)) {
   tbl <- table(cars[, temp_fact$f1[i]], cars[, temp_fact$f2[i]])
   out <- DescTools::UncertCoef(tbl, direction = "column")
   temp_vect <- c(temp_vect, out)
   rm(i, tbl, out)
 }
+
 temp_vect       <- round(temp_vect, 2)
 temp_fact$ucoef <- temp_vect
 temp_fact       <- temp_fact %>% filter(ucoef == 0.00)
@@ -41,16 +43,66 @@ del_columns     <- c('cruise', 'coupe', 'wagon', 'sound', 'buick',
                      'convertible', 'sedan', 'leather', 'pontiac', 
                      'saturn', 'saab')
 cars <- cars %>% select(-del_columns)
-
-rm(temp_vect)
-rm(temp_fact)
+rm(temp_vect, temp_fact, del_columns)
 
 
-
-
-# Data preprocessing includes: identifying near-zero variance variables,
-# identifying correlated predictors, 
 # Split the data into training and testing sets as 80/20.
 set.seed(5627)
-index <- createDataPartition(y = cars$price, p = 0.8, list = F)
+index    <- createDataPartition(y = cars$price, p = 0.8, list = F)
+training <- cars[index, ]
+testing  <- cars[-index,]
+
+
+# Neuralnet requires data preprocessing -- center & scale. We use built in
+# caret function to preprocess the data.
+pre_proc_vals <- preProcess(training, method = c("center", "scale"))
+train_transf  <- predict(pre_proc_vals, training)
+test_trsanf   <- predict(pre_proc_vals, testing)
+
+train_x <- train_transf[, 2:7]
+train_y <- train_transf$price
+test_x  <- test_trsanf[, 2:7]
+test_y  <- test_trsanf$price
+
+
+# Setup a base model -- the simplest model possible is linear regression
+# model. I want to compare it to tuned neuralnet regression with different
+# tuning parameters.
+base_model <- lm(formula = price ~ ., data = train_transf)
+summary(base_model)$r.squared
+
+
+# Train a model using adaptive resampling. Start with adjusting the learning
+# rate. Try to use softplus activation function instead of ReLU -- neuralnet
+# does not recognize ReLU, and will not work with max(k, 0). Softplus is a
+# smooth approximation of ReLU.
+softplus   <- function(x){log(1 + exp(x))}
+adapt_ctrl <- trainControl(method   = "adaptive_cv",
+                           number   = 10,
+                           repeats  = 10,
+                           adaptive = list(min = 5,
+                                           alpha = 0.05,
+                                           method = "gls",
+                                           complete = F),
+                           search   = "random")
+set.seed(5627)
+cars_fit <- train(train_x, train_y,
+                  method    = "neuralnet",
+                  trControl = adapt_ctrl,
+                  tuneGrid  = data.frame(layer1 = 2:3, layer2 = 0, layer3 = 0),
+                  learningrate = 0.20,
+                  act.fct      = softplus,
+                  threshold    = 0.1,
+                  stepmax      = 1e+05)
+cars_fit$bestTune
+cars_fit$finalModel$param
+cars_fit
+
+# TODO: using softplus increased Rsq from 0.75 to 0.76
+# TODO: try adding layer2
+# TODO: try adding expand.grid(.layer1=c(1:4), .layer2=c(0:4), .layer3=c(0))
+# TODO: try running different combinations of LR / layers in databricks -- this
+# is a good idea since the grid search might require a lot of time and res.
+
+
 
